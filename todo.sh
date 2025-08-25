ed4cbe9adabb831d551b7eba5a992f5cc288c832
Sahil Kourav <sahilkourav234@gmail.com>
2025-08-25 22:31:45 +0530

    Docs: Improve usage instructions

--- todo.sh
+++ todo.sh
@@ -3,6 +3,7 @@
 if [ "$1" == "add" ]; then
   echo "$2" >> tasks.txt
   echo "Added task: '$2'"
+  echo "The task is added"
 else
   echo "Todo List Manager"
   echo "Usage: ./todo.sh add 'your task'"
